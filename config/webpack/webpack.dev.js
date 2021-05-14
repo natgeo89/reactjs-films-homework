35ebf45e6fafca5d11db1a5921d30796a34ee8d3
Sakharuk Viktar <natgeogr93@gmail.com>
2021-05-14 20:34:21 +0300

    feat: project basic setup

---
+++ config/webpack/webpack.dev.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  entry: ['webpack-hot-middleware/client', './src/index.tsx'],
+  output: {
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]--[hash:base64:5]',
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+});
